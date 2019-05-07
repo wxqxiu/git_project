cf2fb8ef2a9d20b4eeb8bff469ccb77201030e79
wxq <2766304689@qq.com>
2019-05-07 17:07:27 +0800

    提交到本地仓库

---
+++ Wxq/src/com/student/model/StudentModel.java
@@ -0,0 +1,19 @@
+package com.student.model;
+
+public class StudentModel {
+	
+	public StudentModel(int age, String username, String useremail, int sex, int id) {
+		super();
+		this.age = age;
+		this.username = username;
+		this.useremail = useremail;
+		this.sex = sex;
+		this.id = id;
+	}
+	private int age;
+	private String username;
+	private String useremail;
+	private int sex;
+	private int id;
+
+}
